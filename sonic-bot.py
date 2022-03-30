332995a1bb6b5253da97d688a05260db8066390f
Rachel Logan <56045588+rachelll4@users.noreply.github.com>
2022-03-30 10:52:11 -0400

    update date

--- sonic-bot.py
+++ sonic-bot.py
@@ -1,5 +1,5 @@
 import os
-import datetime
+from datetime import date
 from slack import WebClient
 from slack.errors import SlackApiError
 
@@ -9,13 +9,15 @@ client = WebClient(token = slack_token)
 sonic_release = datetime.datetime(2022,4,8)
 time_to_release = sonic_release - date.today()
 
-try:
-  response = client.chat_postMessage(
-    channel="slack-bots",
-    text="Hello from Sonic! There are "+time_to_release+ " days until Sonic 2 hits theaters. Gotta go fast :checkered_flag:"
-  )
-except SlackApiError as e:
-  # You will get a SlackApiError if "ok" is False
-  assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
-  
-  
+
+if (time_to_release > 0):
+    try:
+      response = client.chat_postMessage(
+        channel="slack-bots",
+        text="Hello from Sonic! There are "+time_to_release+ " days until Sonic 2 hits theaters. Gotta go fast :checkered_flag:"
+      )
+    except SlackApiError as e:
+      # You will get a SlackApiError if "ok" is False
+      assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+      
+      
