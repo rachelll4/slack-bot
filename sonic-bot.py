2c8f24b7a85f1a393ac60dbd0234d956540139de
Rachel Logan <56045588+rachelll4@users.noreply.github.com>
2022-03-30 11:31:16 -0400

    here

--- sonic-bot.py
+++ sonic-bot.py
@@ -22,7 +22,9 @@ if (time_to_release.days > 0):
         
         print("he beat eggman!")
     except SlackApiError as e:
-      # You will get a SlackApiError if "ok" is False
-      assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+        # You will get a SlackApiError if "ok" is False
+        assert e.response["ok"] is False
+        assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+        print(f"Got an error: {e.response['error']}")
       
       
