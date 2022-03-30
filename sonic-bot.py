a8821d350cbcaebfa59c5437e5ccadda0aac326d
Rachel Logan <56045588+rachelll4@users.noreply.github.com>
2022-03-30 11:15:33 -0400

    flushed out

--- sonic-bot.py
+++ sonic-bot.py
@@ -9,13 +9,16 @@ client = WebClient(token = slack_token)
 sonic_release = datetime(2022,4,8)
 time_to_release = sonic_release - datetime(date.today().year,date.today().month, date.today().day)
 
-
-if (time_to_release > 0):
+if (time_to_release.days > 0):
     try:
-      response = client.chat_postMessage(
-        channel="slack-bots",
-        text="Hello from Sonic! There are "+time_to_release+ " days until Sonic 2 hits theaters. Gotta go fast :checkered_flag:"
-      )
+        texty = "Hello from Sonic! There are "+str(time_to_release.days)+ " days until Sonic 2 hits theaters. Gotta go fast"
+        for x in range(15 - time_to_release.days):
+            texty = texty+ ":checkered_flag:"
+    
+        response = client.chat_postMessage(
+            channel="slack-bots",
+            text = texty
+        )
     except SlackApiError as e:
       # You will get a SlackApiError if "ok" is False
       assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
